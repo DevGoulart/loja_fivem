fa6c80408e93d89c77761ac639465d82f9244b96
Kauã Goulart <144181448+DevGoulart@users.noreply.github.com>
2024-09-19 23:16:38 -0300

    teste

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'fallback-key')
 
